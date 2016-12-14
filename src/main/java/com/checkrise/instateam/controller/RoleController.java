f90c677db9e397dc6b4b080f3df50c8d0ff8577b
Grzegorz Kończak <grzegorz.konczak@gmail.com>
2016-12-14 14:01:35 +0100

    Details page for roles added. Users can view details of role and edit name of roles. Small css addition to display edit buttons on roles page.

--- src/main/java/com/checkrise/instateam/controller/RoleController.java
+++ src/main/java/com/checkrise/instateam/controller/RoleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -40,5 +41,13 @@ public class RoleController {
         roleService.save(role);
         return "redirect:/roles";
 
+    }
+
+    // Display details/edit page for specific role
+    @RequestMapping("/roles/{roleId}/details")
+    public String editRoleForm (@PathVariable Long roleId, Model model){
+        Role role = roleService.findById(roleId);
+        model.addAttribute("role", role);
+        return "role/details";
     }
 }
